4e9231f62da7c1a5b341fa1e9abd978c11bd9b16
Zhanglinzhi <mollyzhang0625@outlook.com>
2022-04-19 15:00:22 +0800

    chore: delete default test generated by Gradle

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -1,11 +1,7 @@
 package com.tw.cn.cap.gtb.todo;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        throw new UnsupportedOperationException();
     }
 }
