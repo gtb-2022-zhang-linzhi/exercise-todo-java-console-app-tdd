96c8a97d6e018386d9a2c483ca053c9b0111addc
Zhanglinzhi <mollyzhang0625@outlook.com>
2022-04-19 15:56:41 +0800

    feat: show pure task names in list

--- app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/AppTest.java
@@ -1,6 +1,14 @@
 package com.tw.cn.cap.gtb.todo;
 
-class AppTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+class AppTest {
+    @Test
+    void should_list_existing_tasks() {
+        final var result = new App().run();
+        Assertions.assertEquals(List.of("task 01", "task 02"), result);
     }
 }
