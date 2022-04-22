a7c8e91976da94b362f27da31c3387313c2402ae
Zhanglinzhi <mollyzhang0625@outlook.com>
2022-04-22 11:31:56 +0800

    feat: show result after running

--- app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/App.java
@@ -7,7 +7,7 @@ public class App {
     private final ListCommand listCommand = new ListCommand();
 
     public static void main(String[] args) {
-        throw new UnsupportedOperationException();
+        new App().run().forEach(System.out::println);
     }
 
     public List<String> run() {
