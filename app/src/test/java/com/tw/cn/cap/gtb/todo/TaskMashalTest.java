e58ba4b387cbdb629799a4099abe6fd8a5ff4a8c
Zhanglinzhi <mollyzhang0625@outlook.com>
2022-04-24 16:56:24 +0800

    refactor: apply @ParameterizedTest in TaskMarshalTest

--- app/src/test/java/com/tw/cn/cap/gtb/todo/TaskMashalTest.java
+++ app/src/test/java/com/tw/cn/cap/gtb/todo/TaskMashalTest.java
@@ -1,20 +1,37 @@
 package com.tw.cn.cap.gtb.todo;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TaskMashalTest {
-
-    @Test
-    void should_parse_completed_property_for_task() {
-        final var isCompleted = TaskMarshal.unmarshal(1, "+ + foobar").isCompleted();
-        assertFalse(isCompleted);
+    @ParameterizedTest
+    @MethodSource("lines_and_tasks")
+    void should_mashal_to_plain_text(String line, Task task) {
+        assertEquals(line, TaskMarshal.marshal(task));
     }
 
-    @Test
-    void should_support_name_with_multiple_white_spaces() {
-        final var task = TaskMarshal.unmarshal(1, "+ +     foo   bar   ");
-        assertEquals("    foo   bar   ", task.getName());
+    private static Stream<Arguments> lines_and_tasks() {
+        return Stream.of(
+                Arguments.of("+ foobar", new Task(1, "foo", false)),
+                Arguments.of("+     foo   bar   ", new Task(1, "    foo   bar   ", false))
+        );
     }
+
+//    @Test
+//    void should_parse_completed_property_for_task() {
+//        final var isCompleted = TaskMarshal.unmarshal(1, "+ + foobar").isCompleted();
+//        assertFalse(isCompleted);
+//    }
+//
+//    @Test
+//    void should_support_name_with_multiple_white_spaces() {
+//        final var task = TaskMarshal.unmarshal(1, "+     foo   bar   ");
+//        assertEquals("    foo   bar   ", task.getName());
+//    }
 }
