cd5bf9de1de70e56e32bcf2b64cc22030a8028a5
Zhanglinzhi <mollyzhang0625@outlook.com>
2022-04-24 16:33:56 +0800

    refactor: convert TaskFatory static methods to instance methods

--- app/src/main/java/com/tw/cn/cap/gtb/todo/TaskMarshal.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/TaskMarshal.java
@@ -1,14 +1,12 @@
 package com.tw.cn.cap.gtb.todo;
 
 public class TaskMarshal {
-    private TaskMarshal() {
-    }
 
-    static String marshal(Task task) {
+    String marshal(Task task) {
         return "+ " + task.getName();
     }
 
-    static Task unmarshal(int id, String line) {
+    Task unmarshal(int id, String line) {
         final var fields = line.split(" ", 2);
         final var name = fields[1];
         final var isCompleted = fields[0].equals("x");
--- app/src/main/java/com/tw/cn/cap/gtb/todo/taskRepository.java
+++ app/src/main/java/com/tw/cn/cap/gtb/todo/taskRepository.java
@@ -8,18 +8,26 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class taskRepository {
-    public taskRepository() {
-    }
+
+    private final TaskMarshal taskMarshal = new TaskMarshal();
 
     List<Task> loadTasks() {
         final List<String> lines = readTaskLines();
         final List<Task> tasks = new ArrayList<>();
         for (int i = 0; i < lines.size(); i++) {
-            tasks.add(TaskMarshal.unmarshal(i + 1, lines.get(i)));
+            tasks.add(taskMarshal.unmarshal(i + 1, lines.get(i)));
         }
-        return tasks.stream()
-                .filter(task -> !task.isDeleted())
-                .collect(Collectors.toList());
+        return tasks;
+    }
+
+    public void create(Task task) {
+        try (var bw = Files.newBufferedWriter(Constants.TASKS_FILE_PATH, StandardOpenOption.APPEND)) {
+            final String line = taskMarshal.marshal(task);
+            bw.write(line);
+            bw.newLine();
+        } catch (IOException e) {
+            throw new TodoException();
+        }
     }
 
     List<String> readTaskLines() {
@@ -30,28 +38,4 @@ public class taskRepository {
         }
     }
 
-    public void create(Task task) {
-        try (var bw = Files.newBufferedWriter(Constants.TASKS_FILE_PATH, StandardOpenOption.APPEND)) {
-            final String line = TaskMarshal.marshal(task);
-            bw.write(line);
-            bw.newLine();
-        } catch (IOException e) {
-            throw new TodoException();
-        }
-    }
-
-    public void delete(Integer id) {
-        final var tasks = loadTasks();
-        tasks.stream().filter(task -> task.getId() == id).forEach(Task::delete);
-        try (var bw = Files.newBufferedWriter(Constants.TASKS_FILE_PATH)) {
-            for (Task task : tasks) {
-                final var completedSign = task.isCompleted() ? "x " : "+ ";
-                final var deletedSign = task.isDeleted() ? "x " : "+ ";
-                bw.write(completedSign + deletedSign + task.getName());
-                bw.newLine();
-            }
-        } catch (IOException e) {
-            throw new TodoException();
-        }
-    }
 }
